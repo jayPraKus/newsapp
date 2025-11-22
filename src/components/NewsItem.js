6b424e7c8748a694cf4198ff56c3e39f6e48c526
Jay Prakash Kushwaha <jaaykushwahaa@gmail.com>
2025-11-22 23:50:25 +0545

    Called news api to populate news items

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,15 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageUrl
+                ? "https://cdn.sanity.io/images/s3y3vcno/production/afb72db5244609c27e04ac1bb3950f260319c35e-3840x2160.png?auto=format&w=960&h=540&crop=focalpoint&fit=clip&q=75&fm=jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
